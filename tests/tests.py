b22cf1be01f7aa4aedb99a4b56eea9c9f4e0b021
Chris Down <chris@chrisdown.name>
2014-05-21 14:09:20 +0800

    tests: Test album_id_from_url

---
+++ tests/tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import imurl
+from nose.tools import eq_
+
+
+def test_album_id_from_url_basic():
+    out = imurl.album_id_from_url("http://imgur.com/a/Rbab9")
+    eq_("Rbab9", out)
+
+
+def test_album_id_from_url_with_layout():
+    out = imurl.album_id_from_url("http://imgur.com/a/Rbab9/layout/horizontal")
+    eq_("Rbab9", out)
+
+
+def test_album_id_from_url_with_anchor():
+    out = imurl.album_id_from_url("http://imgur.com/a/Rbab9#0")
+    eq_("Rbab9", out)
+
+
+def test_album_id_from_url_with_layout_and_anchor():
+    out = imurl.album_id_from_url("http://imgur.com/a/Rbab9/layout/horizontal#0")
+    eq_("Rbab9", out)

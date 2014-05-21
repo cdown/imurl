54682b9f660f7dc7615bfcb80b908e5230779d96
Chris Down <chris@chrisdown.name>
2014-05-21 14:11:24 +0800

    album id from url: Support bare album IDs

--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,11 @@ import imurl
 from nose.tools import eq_
 
 
+def test_album_id_from_url_without_url():
+    out = imurl.album_id_from_url("Rbab9")
+    eq_("Rbab9", out)
+
+
 def test_album_id_from_url_basic():
     out = imurl.album_id_from_url("http://imgur.com/a/Rbab9")
     eq_("Rbab9", out)

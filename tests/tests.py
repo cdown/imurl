7bcd44da662e5390342a450cb1417b59cbbe4659
Chris Down <chris@chrisdown.name>
2014-05-21 14:28:19 +0800

    tests: Test filter_image_urls with sample data

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 import imurl
+import json
+import os
 from nose.tools import eq_
+
+
+def relative(path):
+    return os.path.join(os.path.dirname(__file__), path)
 
 
 def test_album_id_from_url_without_url():
@@ -27,3 +33,22 @@ def test_album_id_from_url_with_anchor():
 def test_album_id_from_url_with_layout_and_anchor():
     out = imurl.album_id_from_url("http://imgur.com/a/Rbab9/layout/horizontal#0")
     eq_("Rbab9", out)
+
+def test_filter_image_urls():
+    with open(relative("data/album_response.json")) as api_file:
+        api_response = json.load(api_file)
+
+    eq_([
+        'http://i.imgur.com/24nLu.jpg',
+        'http://i.imgur.com/Ziz25.jpg',
+        'http://i.imgur.com/9tzW6.jpg',
+        'http://i.imgur.com/dFg5u.jpg',
+        'http://i.imgur.com/oknLx.jpg',
+        'http://i.imgur.com/OL6tC.jpg',
+        'http://i.imgur.com/cJ9cm.jpg',
+        'http://i.imgur.com/7BtPN.jpg',
+        'http://i.imgur.com/42ib8.jpg',
+        'http://i.imgur.com/BbwIx.jpg',
+        'http://i.imgur.com/x7b91.jpg',
+    ], list(imurl.filter_image_urls(api_response))
+    )
